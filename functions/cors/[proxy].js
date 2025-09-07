64d208b1ea7944167acdc1b6f60b6ce4baf0d638
DOSBoxOnline <anton.anatolevich@gmail.com>
2025-09-08 02:48:07 +0300

    Update [proxy].js

--- functions/cors/[proxy].js
+++ functions/cors/[proxy].js
@@ -1,22 +1,27 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-
-  // context.params.proxy содержит то, что идёт после /cors/
-  // но нам нужно именно query "?https://..."
   const targetUrl = url.search.substring(1);
 
   if (!targetUrl) {
     return new Response("No URL provided", { status: 400 });
   }
 
-  const response = await fetch(targetUrl);
+  // Важно: пробрасываем заголовки клиента (например, Range)
+  const resp = await fetch(targetUrl, {
+    headers: context.request.headers
+  });
 
-  const newHeaders = new Headers(response.headers);
-  newHeaders.set("Access-Control-Allow-Origin", "*");
-  newHeaders.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
+  // Берём все заголовки из оригинального ответа
+  const headers = new Headers(resp.headers);
 
-  return new Response(response.body, {
-    status: response.status,
-    headers: newHeaders
+  // Добавляем CORS-заголовки
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "*");
+
+  // Возвращаем именно бинарный поток
+  return new Response(resp.body, {
+    status: resp.status,
+    headers
   });
 }
