44da583f94a89c424fc7c7146ab8ec4e77e7ae58
DOSBoxOnline <anton.anatolevich@gmail.com>
2025-09-08 02:26:41 +0300

    fgvhg

--- functions/cors/[proxy].js
+++ functions/cors/[proxy].js
@@ -1,16 +1,15 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // Берём всё после "?" как оригинальный URL
+  // context.params.proxy содержит то, что идёт после /cors/
+  // но нам нужно именно query "?https://..."
   const targetUrl = url.search.substring(1);
 
   if (!targetUrl) {
     return new Response("No URL provided", { status: 400 });
   }
 
-  const response = await fetch(targetUrl, {
-    headers: context.request.headers
-  });
+  const response = await fetch(targetUrl);
 
   const newHeaders = new Headers(response.headers);
   newHeaders.set("Access-Control-Allow-Origin", "*");
